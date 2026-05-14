4534b08e3dffaa65478b6fd104d120b02c391487
s-haider10 <sh6070@nyu.edu>
2026-05-13 22:04:10 -0400

    add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 // This is a USER site (s-haider10.github.io) deployed at root.
 // If you ever migrate to a project subpath (e.g. /website/), set `base: '/website'`.
 export default defineConfig({
-  site: 'https://s-haider10.github.io',
+  site: "https://s-haider10.github.io",
+  integrations: [sitemap()],
 });
